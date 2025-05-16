1908dab6b7daa54b5dfdc7da4a0a0fa23f858678
oklarsson <larssonot@gmail.com>
2025-05-16 16:40:23 +0200

    Uppdatera main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def on_forever():
-    pass
+
+    def on_gesture_shake():
+        result = randint(1,6)
+        print(result)
+    input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function on_forever() {
-    
+    input.onGesture(Gesture.Shake, function on_gesture_shake() {
+        let result = randint(1, 6)
+        console.log(result)
+    })
 })
